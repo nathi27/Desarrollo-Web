e8ccd874e9f6585af98172914d65716e6e050caa
nathi27 <nathaliecham04@gmail.com>
2025-11-13 18:17:26 -0600

    Pagina principal
    Hay que darle más color y forma a como está en el prototipo

--- src/test/java/com/pasteleria/PasteleriaApplicationTests.java
+++ src/test/java/com/pasteleria/PasteleriaApplicationTests.java
@@ -1,13 +1,13 @@
 package com.pasteleria;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PasteleriaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
